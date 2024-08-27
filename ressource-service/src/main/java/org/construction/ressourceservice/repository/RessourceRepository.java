b8e297913e9165d762fc3d0678620122c02d096d
BAOUCHbaSalma <baouch.salma0@gmail.com>
2024-08-27 16:58:31 +0100

    j'ai commencee la configuration du micro service gateway

--- ressource-service/src/main/java/org/construction/ressourceservice/repository/RessourceRepository.java
+++ ressource-service/src/main/java/org/construction/ressourceservice/repository/RessourceRepository.java
@@ -2,6 +2,9 @@ package org.construction.ressourceservice.repository;
 
 import org.construction.ressourceservice.model.Ressource;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
 
 public interface RessourceRepository extends JpaRepository<Ressource,Integer> {
 }
--- tache-service/src/main/java/org/construction/tacheservice/repository/TacheRepository.java
+++ tache-service/src/main/java/org/construction/tacheservice/repository/TacheRepository.java
@@ -2,6 +2,8 @@ package org.construction.tacheservice.repository;
 
 import org.construction.tacheservice.model.Tache;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface TacheRepository extends JpaRepository<Tache,Integer> {
 }
