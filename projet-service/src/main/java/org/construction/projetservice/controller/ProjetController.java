c2530a68d6bb346ee77f8bb5b5e1a4ba1bfcb091
BAOUCHbaSalma <baouch.salma0@gmail.com>
2024-08-28 14:50:23 +0100

    j'ai presque terminee la configuration du controller de projet

--- projet-service/src/main/java/org/construction/projetservice/controller/ProjetController.java
+++ projet-service/src/main/java/org/construction/projetservice/controller/ProjetController.java
@@ -2,8 +2,11 @@ package org.construction.projetservice.controller;
 
 import org.construction.projetservice.model.Projet;
 import org.construction.projetservice.repository.ProjetRepository;
+import org.construction.projetservice.service.ProjetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -12,11 +15,17 @@ import java.util.List;
 public class ProjetController {
 
     @Autowired
-    private ProjetRepository projetRepository;
+    private ProjetService projetService;
 
 
     @GetMapping("/projects")
     public List<Projet> findAll(){
-        return projetRepository.findAll();
+        return projetService.showProjet();
     }
+
+    @PostMapping("/projets")
+    public Projet addProjet(@RequestBody Projet projet){
+        return projetService.addProjet(projet);
+    }
+    
 }
